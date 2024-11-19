5d44f481516a7d4e8e876be102178174d5e0f861
Mohshalaby92 <mohammedshalaby92@gmail.com>
2024-11-19 15:32:57 -0800

    sql

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- deletes the database hbtn_0c_0, if doesn,t exist, code must not fail
+-- deletes the database hbtn_0c_0, if doesn,t exist, code must not fail
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Creates table 'first_table' in database
+-- Creates table 'first_table' in database
+CREATE TABLE first_table(id INT, name VARCHAR(256));
