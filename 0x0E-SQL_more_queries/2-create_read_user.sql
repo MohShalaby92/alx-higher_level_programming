0d43e271aef8008d8aa8aee6fdaebb594d369e31
Mohshalaby92 <mohammedshalaby92@gmail.com>
2024-11-20 10:54:08 -0800

    2&3

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,8 @@
---  creates the database hbtn_0d_2 and the user user_0d_2
--- create database
+-- create a database hbtn_0d_2 and the user user_0d_2
+-- creates a new database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- create user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
--- grant privieges
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%'
+-- create a new user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grant priviledges to this user
+GRANT SELECT ON hbtn_0d_2.*
+TO user_0d_2@localhost;
--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- creates the table force_name on your MySQL server.
 -- create table
-CREATE TABLE IF NOT EXIST force_name (
-id INT
+CREATE TABLE IF NOT EXISTs force_name (
+id INT,
 name VARCHAR(256) NOT NULL
 );
