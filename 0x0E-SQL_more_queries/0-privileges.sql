458bcc665609370da2a943367b61531162cc15ba
Mohshalaby92 <mohammedshalaby92@gmail.com>
2024-11-20 05:56:50 -0800

    create

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- lists all privileges of the MySQL users
+-- list 'user_od_1 privileges
+SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
+-- list 'user_od_2 privileges
+SHOW GRANTS FOR 'user_0d_2' @ 'localhost';
